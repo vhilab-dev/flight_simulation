9e8f4ab9db1c209876dd738ddf3b69f70001ca22
DavidCante <dcante@stanford.edu>
2019-12-05 04:54:02 -0800

    fix git

---
+++ Assets/Instructions.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * This code takes control of the audio instructions in the beginning of the scene.
+ * To bypass this, press 'f'.
+ *
+ */
+public class Instructions : MonoBehaviour
+{
+    public AudioSource fly1;
+    public AudioSource fly2;
+    public AudioSource fly3;
+    public AudioSource fly4;
+    public AudioSource fly5;
+    public AudioSource fly6;
+
+    public GameObject leftArrow;
+    public GameObject rightArrow;
+    public GameObject lController;
+    public GameObject rController;
+    public GameObject hmd;
+
+    private bool finished1 = false;
+    private bool finished2 = false;
+    private bool finished3 = false;
+    private bool finished4 = false;
+    private bool finished5 = false;
+    private bool finished6 = false;
+    private bool flipped = false;
+    private float timePassed = 0f;
+    private Color color;
+    private Renderer leftRenderer;
+    private Renderer rightRenderer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = leftArrow.GetComponent<Renderer>().material.color;
+        leftRenderer = leftArrow.GetComponent<Renderer>();
+        rightRenderer = rightArrow.GetComponent<Renderer>();
+        leftArrow.SetActive(false);
+        rightArrow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed >= 2f && !finished1)
+        {
+            if (!fly1.isPlaying)
+            {
+                fly1.Play();
+            }
+            if (timePassed >= 15f)
+            {
+                Debug.Log("Got to showing arrow");
+                leftArrow.SetActive(true);
+                rightArrow.SetActive(true);
+            }
+            if ((lController.transform.position)[1] - (hmd.transform.position)[1] > 0f)
+            {
+                Debug.Log("Got to showing arrow color change");
+                leftRenderer.material.SetColor("_Color", Color.blue);
+                rightRenderer.material.SetColor("_Color", Color.blue);
+            }
+            else
+            {
+                Debug.Log("Got to showing arrow color change");
+                leftRenderer.material.SetColor("_Color", color);
+                rightRenderer.material.SetColor("_Color", color);
+            }
+        }
+        if (timePassed >= 24f && !finished2)
+        {
+            if (!fly2.isPlaying)
+            {
+                fly2.Play();
+            }
+            finished1 = true;
+        }
+        if (timePassed >= 27f && !finished3)
+        {
+            if (!fly3.isPlaying)
+            {
+                fly3.Play();
+            }
+            finished2 = true;
+            if (timePassed >= 28f && !flipped)
+            {
+                leftArrow.transform.Rotate(-90, 0, 0, Space.Self);
+                rightArrow.transform.Rotate(90, 0, 0, Space.Self);
+                flipped = !flipped;
+            }
+            if (Vector3.Distance(lController.transform.position, rController.transform.position) > 1.2f)
+            {
+                leftRenderer.material.SetColor("_Color", Color.blue);
+                rightRenderer.material.SetColor("_Color", Color.blue);
+            }
+            else
+            {
+                leftRenderer.material.SetColor("_Color", color);
+                rightRenderer.material.SetColor("_Color", color);
+            }
+        }
+        if (timePassed >= 33f && !finished4)
+        {
+            if (!fly4.isPlaying)
+            {
+                fly4.Play();
+            }
+            finished3 = true;
+            if (timePassed >= 33f && flipped)
+            {
+                leftArrow.transform.Rotate(180, 0, 0, Space.Self);
+                rightArrow.transform.Rotate(-180, 0, 0, Space.Self);
+                flipped = !flipped;
+            }
+            if (Vector3.Distance(lController.transform.position, rController.transform.position) < 0.25f)
+            {
+                leftRenderer.material.SetColor("_Color", Color.blue);
+                rightRenderer.material.SetColor("_Color", Color.blue);
+            }
+            else
+            {
+                leftRenderer.material.SetColor("_Color", color);
+                rightRenderer.material.SetColor("_Color", color);
+            }
+        }
+        if (timePassed >= 39f && !finished5)
+        {
+            if (!fly5.isPlaying)
+            {
+                fly5.Play();
+            }
+            finished4 = true;
+            if (!flipped)
+            {
+                leftArrow.transform.Rotate(90, 0, 0, Space.Self);
+                rightArrow.transform.Rotate(-90, 0, 0, Space.Self);
+                flipped = !flipped;
+            }
+            if ((lController.transform.position)[1] - (hmd.transform.position)[1] < -0.75f)
+            {
+                leftRenderer.material.SetColor("_Color", Color.blue);
+                rightRenderer.material.SetColor("_Color", Color.blue);
+            }
+            else
+            {
+                leftRenderer.material.SetColor("_Color", color);
+                rightRenderer.material.SetColor("_Color", color);
+            }
+
+        }
+        if (timePassed >= 44f && !finished6)
+        {
+            if (!fly6.isPlaying)
+            {
+                fly6.Play();
+            }
+            finished5 = true;
+            leftArrow.SetActive(false);
+            rightArrow.SetActive(false);
+        }
+        if (timePassed >= 50f)
+        {
+            finished6 = true;
+        }
+
+        if (Input.GetKeyDown("f"))
+        {
+            fly1.Stop();
+            fly2.Stop();
+            fly3.Stop();
+            fly4.Stop();
+            fly5.Stop();
+            fly6.Stop();
+            finished1 = true;
+            finished2 = true;
+            finished3 = true;
+            finished4 = true;
+            finished5 = true;
+            finished6 = true;
+        }
+    }
+}
---
+++ Assets/flight.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+/*
+ * This code allows for a participant to fly throughout a city either as ironman or superman.
+ * To allow the user to begin flying, press the 'f' key.
+ * Easter Egg. To fly as as ironman, press 's'.
+ */
+public class flight : MonoBehaviour
+{
+    public GameObject lController;
+    public GameObject rController;
+    public GameObject hmd;
+    public GameObject leftArrow;
+    public GameObject rightArrow;
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+    public SteamVR_Input_Sources hands;
+    public SteamVR_Action_Boolean trigger;
+    public AudioSource flightAudio;
+    public float thrust = 7.0f;
+    public float cap = 15.0f;
+    public float maxSpeed = 5.0f;
+    public float speedScalar = 6.0f;
+
+
+
+    private Rigidbody rb;
+    private bool isSuperman = true; //Determines whether user flies as superman or ironman. Press 's' to switch.
+    private bool canStart = false;
+    private float timeLeft = 50.0f;
+    private bool flipped = true;
+    private float multiplier = -1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft < -2 || Input.GetKeyDown("f"))
+        {
+            // User cannot begin flying until experimenter presses the f key.
+            leftArrow.SetActive(false);
+            rightArrow.SetActive(false);
+            canStart = true;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            // User will onyl fly as in the traditional flight simulation until experimenter presses the 's' key.
+            isSuperman = !isSuperman;
+            rb.velocity = Vector3.zero;
+        }
+
+        timeLeft -= Time.deltaTime;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (canStart == false)
+        {
+            //pass do nothing because we have not started
+        }
+        // ironman flight simulation
+        else if (isSuperman == false)
+        {
+
+            rb.useGravity = true;
+            if (rb.velocity.magnitude > cap)
+            {
+                rb.velocity = rb.velocity.normalized * cap;
+            }
+
+            // hold triggers to activate thrusters. Each hand is independently controlled.  
+            addThruster(leftHand, lController.transform.rotation);
+            addThruster(rightHand, rController.transform.rotation);
+            playSoundsRegular(cap, rb.velocity.magnitude, isSuperman, flightAudio);
+        }
+        // Superman flight simulation.
+        else
+        {
+            rb.useGravity = false;
+
+            float distance = Vector3.Distance(lController.transform.position, rController.transform.position);
+            float speed = Mathf.Max(maxSpeed - distance * speedScalar, 0.5f);
+
+            Vector3 heading = (lController.transform.position + rController.transform.position) / 2.0f - hmd.transform.position;
+            Vector3 direction = heading.normalized;
+
+            Vector3 speedVector = speed * direction;
+            rb.velocity = speedVector;
+            playSoundsRegular(maxSpeed, speed, isSuperman, flightAudio);
+        }
+    }
+
+    private void playSoundsRegular(float maxSpeed, float speed, bool isSuperman, AudioSource flightAudio)
+    {
+
+        //Allows user to control volume of flight. Volume of flight is scaled by how fast user is flying.
+
+        if (isSuperman || (!isSuperman && trigger.GetState(hands)))
+        {
+            float volume = speed / (1.5f * maxSpeed);
+            if (!isSuperman)
+            {
+                volume = Mathf.Max(0.2f, volume);
+            }
+            flightAudio.volume = volume;
+            if (!flightAudio.isPlaying)
+            {
+                flightAudio.Play();
+            }
+        }
+    }
+
+
+    private void addThruster(SteamVR_Input_Sources hand, Quaternion rot)
+    {
+        if (trigger.GetState(hand))
+        {
+            Vector3 forward = rot * Vector3.forward;
+            rb.AddForce(-1.0f * forward * thrust);
+        }
+    }
+}
